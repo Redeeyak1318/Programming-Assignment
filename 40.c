4f1b7ba5afb5dd0648578b8867e15f6e9322a8f2
Raktim Sonowal <Redeeyak1318@users.noreply.github.com>
2026-05-20 00:33:07 +0530

    Final Tweak

--- 40.c
+++ 40.c
@@ -11,21 +11,23 @@ int main()
     {
         scanf("%d", &a[i]);
     }
-    printf("The elements of the array are: ");
+    printf("The elements of the array are: [");
     for(i=0;i<n;i++)
     {
         printf("%d ", a[i]);
     }
+    printf("]");
     printf("\nEnter the position of the element to be deleted: ");
     scanf("%d", &pos);
     for(i=pos-1;i<n-1;i++)
     {
         a[i] = a[i+1];
     }
-    printf("The array after deletion is: ");
+    printf("The array after deletion is: [");
     for(i=0;i<n-1;i++)
     {
         printf("%d ", a[i]);
     }
+    printf("]");
     return 0;
 }
